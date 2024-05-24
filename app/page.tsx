d0bba50594efe9c0fd46c5f46838c92c370e2104
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-05-24 18:07:49 +0530

    added the mirrored state

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,30 @@
-"use client"
-import React, { useRef } from 'react'
-import Webcam from 'react-webcam';
+"use client";
+import React, { useRef, useState } from "react";
+import Webcam from "react-webcam";
 
-type Props = {}
+type Props = {};
 
 const Home = (props: Props) => {
   const webcamRef = useRef<Webcam>(null);
 
+  // State Variables
+  const [mirrored, setmirrored] = useState<boolean>(false);
 
   return (
     <div className="flex h-screen">
       <div className="relative">
         <div className="relative h-screen w-full">
-          <Webcam ref={webcamRef}/>
+          <Webcam ref={webcamRef} 
+            mirrored={mirrored} 
+            className="h-full w-full object-contain p-2"
+          />
+          <canvas ref={canvasRef}>
+
+          </canvas>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
