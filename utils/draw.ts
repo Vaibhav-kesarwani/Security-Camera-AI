515e76f8f3e485252bc1bbe7910121576d6cb82a
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-06-08 12:41:42 +0530

    chnage the parsed tensorflow file conditiion

--- utils/draw.ts
+++ utils/draw.ts
@@ -10,6 +10,27 @@ export function drawOnCanvas(
     const { class: name, bbox, score } = detectedObject;
     const [x, y, width, height] = bbox;
 
-   
+    if (ctx) {
+      ctx.beginPath();
+
+      // styling
+      ctx.fillStyle = name === "person" ? "#FF0F0F" : "#00B612";
+      ctx.globalAlpha = 0.4;
+
+      mirrored
+        ? ctx.roundRect(ctx.canvas.width - x, y, -width, height, 8)
+        : ctx.roundRect(x, y, width, height, 8);
+
+      // draw stroke or fill
+      ctx.fill();
+
+      // text styling
+      ctx.font = "12px Courier New";
+      ctx.fillStyle = "black";
+      ctx.globalAlpha = 1;
+      mirrored
+        ? ctx.fillText(name, ctx.canvas.width - x - width + 10, y + 20)
+        : ctx.fillText(name, x + 10, y + 20);
+    }
   });
 }
