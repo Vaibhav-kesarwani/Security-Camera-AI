2f947552e5224545eb4e7de51bfbd0f396a95554
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-06-08 13:52:39 +0530

    change to some socials

--- components/social-links.tsx
+++ components/social-links.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faPhoenixFramework, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const SocialMediaLinks = () => {
     return (
@@ -11,9 +11,9 @@ const SocialMediaLinks = () => {
             <a href="https://www.linkedin.com/in/vaibhav-kesarwani-9b5b35252/" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faLinkedin} size="2x" />
             </a>
-            <a href="https://vaibhavkesarwani.vercel.app/" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faPhoenixFramework} size="2x" />
-            </a>
+            {/* <a href="https://vaibhavkesarwani.vercel.app/" target="_blank" rel="noopener noreferrer"> */}
+                {/* <FontAwesomeIcon icon={faGlobe} className='fa-solid fa-globe' size="2x" /> */}
+            {/* </a> */}
         </div>
     );
 };
